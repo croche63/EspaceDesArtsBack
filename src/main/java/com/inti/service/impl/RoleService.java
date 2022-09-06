f60b5832ded954cf462805663559aa5922c4d8a2
GuillaumeSignoret <guillaume.signoret@outlook.com>
2022-09-06 16:12:39 +0200

    Update RoleService.java

--- src/main/java/com/inti/service/impl/RoleService.java
+++ src/main/java/com/inti/service/impl/RoleService.java
@@ -5,28 +5,28 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.inti.dao.RoleDAO;
 import com.inti.entities.Role;
+import com.inti.repositories.RoleRepository;
 import com.inti.service.interfaces.IRoleService;
 
 @Service
 public class RoleService implements IRoleService {
 	@Autowired
-	RoleDAO RoleDAO;
+	RoleRepository RoleRepository;
 
 	@Override
 	public List<Role> findAll() {
-		return RoleDAO.findAll();
+		return RoleRepository.findAll();
 	}
 
 	@Override
 	public Role save(Role Role) {
-		return RoleDAO.save(Role);
+		return RoleRepository.save(Role);
 	}
 
 	@Override
 	public void delete(Long id) {
-		RoleDAO.deleteById(id);
+		RoleRepository.deleteById(id);
 
 	}
 
