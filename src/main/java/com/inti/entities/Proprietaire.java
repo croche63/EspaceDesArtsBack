f59b37116a5cb786d3d08ca6bf98f4ede6431c74
GuillaumeSignoret <guillaume.signoret@outlook.com>
2022-09-08 17:46:35 +0200

    Ajout d'annotations Transcient
    pour éviter les loop

--- src/main/java/com/inti/entities/Proprietaire.java
+++ src/main/java/com/inti/entities/Proprietaire.java
@@ -4,17 +4,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Transient;
 
 @Entity
 @DiscriminatorValue("Proprietaire")
 public class Proprietaire extends Utilisateur {
 
+	@Transient
 	@OneToMany(mappedBy = "proprietaire")
 	private Set<Reservation> reservations = new HashSet<>();
-
+	
+	@Transient
 	@OneToOne(mappedBy = "proprietaire")
 	private SalleExposition salleExposition;
 
