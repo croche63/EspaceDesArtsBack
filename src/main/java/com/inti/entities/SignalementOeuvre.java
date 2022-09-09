5ef5825da61c0c9df984bf771f40ceb2a00ab0eb
croche63 <111734808+croche63@users.noreply.github.com>
2022-09-09 14:54:05 +0200

    Json ignore fix

--- src/main/java/com/inti/entities/SignalementOeuvre.java
+++ src/main/java/com/inti/entities/SignalementOeuvre.java
@@ -35,7 +35,6 @@ public class SignalementOeuvre implements Serializable {
 
 	@ManyToOne(fetch=FetchType.EAGER)
 	@JoinColumn(name = "idUtilisateur")
-	@JsonIgnore
 	private Utilisateur utilisateur;
 
 	public SignalementOeuvre() {
