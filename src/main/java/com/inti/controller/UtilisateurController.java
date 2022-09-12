d0ba554c9aa5e7f4929af76ed54836901a008adb
croche63 <111734808+croche63@users.noreply.github.com>
2022-09-12 16:34:05 +0200

    Suppr. image, ajout role controlleur utilisateur

--- src/main/java/com/inti/controller/UtilisateurController.java
+++ src/main/java/com/inti/controller/UtilisateurController.java
@@ -1,6 +1,7 @@
 package com.inti.controller;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.inti.entities.Role;
 import com.inti.entities.Utilisateur;
 import com.inti.service.interfaces.IUtilisateurService;
 
@@ -37,40 +39,39 @@ public class UtilisateurController {
 		return utilisateurService.findOne(id);
 	}
 
-//	@PostMapping("/utilisateurs") 
-//	public Utilisateur saveUtilisateur(@RequestBody Utilisateur utilisateur) {
-//		return utilisateurService.save(utilisateur);
-//	}
-	// Nouvelle méthode de sauvegarde:
-	@PostMapping("/utilisateurs")
-	public String saveUtilisateur(@RequestParam("nom") String nom, @RequestParam("prenom") String prenom,
-			@RequestParam("username") String username, @RequestParam("password") String password,
-			@RequestParam("email") String email,
-			@RequestParam("numeroTel") String numeroTel,
-		//	@RequestParam("roles") Set<Role> roles, 
-			@RequestParam(name="image", required = false) MultipartFile image) {
-		try {
-			Utilisateur currentUser = new Utilisateur();
-			currentUser.setNom(nom);
-			currentUser.setPrenom(prenom);
-			currentUser.setUsername(username);
-			currentUser.setPassword(passwordEncoder.encode(password));
-			currentUser.setEmail(email);
-			currentUser.setNumeroTel(numeroTel);
-		//	currentUser.setRoles(roles);
-			//Sauvegarder image que si on en a envoyé une
-			if(image != null) {
-				currentUser.setImage(image.getBytes());
-			}
-			utilisateurService.save(currentUser);
-
-			return "User created successfully! ";
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			return "Fail! maybe you had uploaded the file before or the file's size > 500KB";
-			// TODO: handle exception
-		}
+	@PostMapping("/utilisateurs") 
+	public Utilisateur saveUtilisateur(@RequestBody Utilisateur utilisateur) {
+		return utilisateurService.save(utilisateur);
 	}
+	// Nouvelle méthode de sauvegarde:
+	/*
+	 * @PostMapping("/utilisateurs") public String
+	 * saveUtilisateur(@RequestParam("nom") String nom, @RequestParam("prenom")
+	 * String prenom,
+	 * 
+	 * @RequestParam("username") String username, @RequestParam("password") String
+	 * password,
+	 * 
+	 * @RequestParam("email") String email,
+	 * 
+	 * @RequestParam("numeroTel") String numeroTel,
+	 * 
+	 * @RequestParam("roles") Set<Role> roles,
+	 * 
+	 * @RequestParam(name="image", required = false) MultipartFile image) { try {
+	 * Utilisateur currentUser = new Utilisateur(); currentUser.setNom(nom);
+	 * currentUser.setPrenom(prenom); currentUser.setUsername(username);
+	 * currentUser.setPassword(passwordEncoder.encode(password));
+	 * currentUser.setEmail(email); currentUser.setNumeroTel(numeroTel);
+	 * currentUser.setRoles(roles); //Sauvegarder image que si on en a envoyé une
+	 * if(image != null) { currentUser.setImage(image.getBytes()); }
+	 * utilisateurService.save(currentUser);
+	 * 
+	 * return "User created successfully! "; } catch (Exception ex) {
+	 * ex.printStackTrace(); return
+	 * "Fail! maybe you had uploaded the file before or the file's size > 500KB"; //
+	 * TODO: handle exception } }
+	 */
 
 	@DeleteMapping("/utilisateurs/{idUtilisateur}")
 	public void deleteUtilisateur(@PathVariable("idUtilisateur") Long id) {
