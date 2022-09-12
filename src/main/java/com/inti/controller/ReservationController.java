4262d70c48ab6702d67067a146384c65718d07f7
SaraSousa26 <sarafilipa26@gmail.com>
2022-09-12 10:38:54 +0200

    ajout idSalle dans Reservation controller

--- src/main/java/com/inti/controller/ReservationController.java
+++ src/main/java/com/inti/controller/ReservationController.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.entities.Artiste;
 import com.inti.entities.Reservation;
+import com.inti.entities.SalleExposition;
 import com.inti.service.interfaces.IArtisteService;
 import com.inti.service.interfaces.IReservationService;
+import com.inti.service.interfaces.ISalleExpositionService;
 
 @RestController
 @CrossOrigin
@@ -25,15 +27,22 @@ public class ReservationController {
 	@Autowired
 	IArtisteService artisteService;
 	
+	@Autowired
+	ISalleExpositionService salleExpositionService;
+	
 	@GetMapping("/Reservations")
 	public List<Reservation> findAll(){
 		return reservationService.findAll();
 	}
 	
-	@PostMapping("/Reservations/{username}")
-	public Reservation saveReservation(@RequestBody Reservation reservation, @PathVariable("username") String username) {
+	@PostMapping("/Reservations/{username}/{idSalle}")
+	public Reservation saveReservation(@RequestBody Reservation reservation, @PathVariable("username") String username, @PathVariable("idSalle") String id) {
 		Artiste artiste = artisteService.findByUsername(username);
 		reservation.setArtiste(artiste);
+		
+		Long idSalleExp = Long.parseLong(id);
+		SalleExposition salleExposition = salleExpositionService.findById(idSalleExp);
+		reservation.setSalleExposition(salleExposition);
 		return reservationService.save(reservation, username);
 	}
 	
