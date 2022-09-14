0238c2a1c007149c348be8cef593b706531b1d14
SaraSousa26 <sarafilipa26@gmail.com>
2022-09-14 13:39:20 +0200

    methode save salleExpo avec image et username

--- src/main/java/com/inti/entities/Proprietaire.java
+++ src/main/java/com/inti/entities/Proprietaire.java
@@ -1,7 +1,10 @@
 package com.inti.entities;
 
+import java.util.List;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -10,6 +13,9 @@ public class Proprietaire extends Utilisateur {
 
 	@OneToOne(mappedBy = "proprietaire")
 	private SalleExposition salleExposition;
+	
+	@OneToMany(mappedBy = "proprietaire")
+	private List<SalleVirtuelle> SalleVirtuelles;
 
 	public Proprietaire() {
 		super();
@@ -22,5 +28,15 @@ public class Proprietaire extends Utilisateur {
 	public void setSalleExposition(SalleExposition salleExposition) {
 		this.salleExposition = salleExposition;
 	}
+
+	public List<SalleVirtuelle> getSalleVirtuelles() {
+		return SalleVirtuelles;
+	}
+
+	public void setSalleVirtuelles(List<SalleVirtuelle> salleVirtuelles) {
+		SalleVirtuelles = salleVirtuelles;
+	}
+	
+	
 
 }
